3056ccc9cd4d68bd75006bec2fe22feb3ffd5c66
Patrick Brito <patrickbritokun530@gmail.com>
2023-03-16 16:35:29 -0300

    cadastro de pacientes em Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.scss']
+})
+export class CadastroComponent {
+  selector = 'menu-overview-example' ;
+  templateUrl = 'menu-overview-example.html' ;
+}
+export class MenuOverviewExample {}
